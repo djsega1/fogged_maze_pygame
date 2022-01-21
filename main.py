d0fc594687b1b8d99a211d879f534e54f6a68c61
DJsega1 <DJsega1@yandex.ru>
2022-01-21 19:13:08 +0300

    Added parent classes and walls collision

--- main.py
+++ main.py
@@ -2,32 +2,73 @@ import pygame
 import csv
 from math import ceil
 
+
+class SpriteGroup(pygame.sprite.Group):
+
+    def __init__(self):
+        super().__init__()
+
+    def get_event(self, event):
+        for sprite in self:
+            sprite.get_event(event)
+
+
+class Sprite(pygame.sprite.Sprite):
+
+    def __init__(self, group):
+        super().__init__(group)
+
+    def get_event(self, event):
+        pass
+
+
 COEF_X, COEF_Y = 0.05, 0.08
 SCREEN = pygame.display.set_mode((0, 0), pygame.RESIZABLE)
 WIDTH, HEIGHT = SCREEN.get_size()
 SPRITES_WIDTH, SPRITES_HEIGHT = WIDTH * COEF_X, HEIGHT * COEF_Y
+user, walls = SpriteGroup(), SpriteGroup()
 
 
-# Класс Монтгомери
-class Monty(pygame.sprite.Sprite):
-    image = pygame.image.load("data\\BryceRight1.png")
-    image = pygame.transform.scale(image, (SPRITES_WIDTH, SPRITES_HEIGHT))
-    rect = image.get_rect()
+class Monty(Sprite):
 
-    def __init__(self, *group):
-        super().__init__(*group)
+    def __init__(self, x, y):
+        super().__init__(user)
+        self.image = pygame.transform.scale(pygame.image.load("data\\BryceRight1.png"),
+                                            (SPRITES_WIDTH, SPRITES_HEIGHT))
+        self.rect = self.image.get_rect()
+        self.rect.left = x
+        self.rect.top = y
+        self.last = self.rect.copy()
 
-    def update(self):
-        pass
+    def update(self, key):
+        self.last = self.rect.copy()
+        if key[pygame.K_w]:
+            self.rect.top -= 10
+            if pygame.sprite.spritecollideany(self, walls):
+                self.rect.top += 10
+        if key[pygame.K_s]:
+            self.rect.top += 10
+            if pygame.sprite.spritecollideany(self, walls):
+                self.rect.top -= 10
+        if key[pygame.K_a]:
+            self.rect.left -= 10
+            if pygame.sprite.spritecollideany(self, walls):
+                self.rect.left += 10
+        if key[pygame.K_d]:
+            self.rect.left += 10
+            if pygame.sprite.spritecollideany(self, walls):
+                self.rect.left -= 10
 
 
-class Walls(pygame.sprite.Sprite):
-    image = pygame.image.load("data\\Wall.png")
-    image = pygame.transform.scale(image, (SPRITES_WIDTH, SPRITES_HEIGHT))
-    rect = image.get_rect()
+class Walls(Sprite):
 
-    def __init__(self, *group):
-        super().__init__(*group)
+    def __init__(self, x, y):
+        super().__init__(walls)
+        self.image = pygame.transform.scale(pygame.image.load("data\\Wall.png"),
+                                            (SPRITES_WIDTH, SPRITES_HEIGHT))
+        self.rect = self.image.get_rect()
+        self.rect.left = x
+        self.rect.top = y
 
     def update(self):
         pass
@@ -38,45 +79,18 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     fps = 60
-    all_sprites = pygame.sprite.Group()
-    player = Monty()
-    walls = list()
-    # стены по периметру
-    for i in range(ceil(WIDTH / SPRITES_WIDTH)):
-        wall = Walls(all_sprites)
-        wall.rect.left = i * SPRITES_WIDTH
-        walls.append(wall)
-        wall = Walls(all_sprites)
-        wall.rect.left = i * SPRITES_WIDTH
-        wall.rect.top = HEIGHT - SPRITES_HEIGHT
-        walls.append(wall)
-    for i in range(ceil(HEIGHT / SPRITES_HEIGHT)):
-        wall = Walls(all_sprites)
-        wall.rect.top = i * SPRITES_HEIGHT
-        walls.append(wall)
-        wall = Walls(all_sprites)
-        wall.rect.left = WIDTH - SPRITES_WIDTH
-        wall.rect.top = i * SPRITES_HEIGHT
-        walls.append(wall)
-    # --
-    all_sprites.add(player)
+    Monty(500, 500)
+    Walls(200, 200)
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        key = pygame.key.get_pressed()
-        if key[pygame.K_w]:
-            player.rect.top -= 10
-        if key[pygame.K_s]:
-            player.rect.top += 10
-        if key[pygame.K_a]:
-            player.rect.left -= 10
-        if key[pygame.K_d]:
-            player.rect.left += 10
-        SCREEN.fill((0, 0, 0))
-        all_sprites.draw(SCREEN)
+        user.update(pygame.key.get_pressed())
         pygame.event.pump()
+        SCREEN.fill((0, 0, 0))
+        user.draw(SCREEN)
+        walls.draw(SCREEN)
         clock.tick(30)
         pygame.display.flip()
     pygame.quit()
